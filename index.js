e34d0f567cdd5672376d2f131f7826be3ad1f50b
Rahat-khan117 <rahatkhan015002@gmail.com>
2025-03-02 22:28:56 +0600

    all most

--- index.js
+++ index.js
@@ -1,5 +1,213 @@
-document.getElementById("discover_btn").addEventListener("click",
-    function (question) {
-        window.location.href="main.html"
+document
+  .getElementById("discover_btn")
+  .addEventListener("click", function (question) {
+    window.location.href = "main.html";
+  });
+document
+  .getElementById("completed-1")
+  .addEventListener("click", function (event) {
+    let taskNumber = document.getElementById("task_n").innerText;
+    let convertedTaskNumber = parseFloat(taskNumber);
+    let taskSum = convertedTaskNumber - 1;
+    document.getElementById("task_n").innerText = taskSum;
+
+    let navTask = document.getElementById("nav_task").innerText;
+    let convertedNavTask = parseInt(navTask);
+    let navSum = convertedNavTask + 1;
+    document.getElementById("nav_task").innerText = navSum;
+
+    document.getElementById("completed-1").style.backgroundColor = "#8D8E9A";
+
+    alert("Board updated successfully")
+
+    // add box
+
+    let box = document.createElement("div");
+    box.style.width = "300px";
+    box.style.height = "100px";
+    box.style.backgroundColor = "#F4F7FF";
+    box.style.margin = "25px";
+    box.style.borderRadius = "15px";
+    box.textContent = `You have Complete The Task Fix Mobile Button Issue at ` + new Date().toLocaleTimeString();
+    box.style.color = "gray";
+    box.style.display = "flex"; 
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.textAlign= "center";
+    document.getElementById("container").appendChild(box);
+  });
+
+document
+  .getElementById("completed-2")
+  .addEventListener("click", function (event) {
+    let taskNumber = document.getElementById("task_n").innerText;
+    let convertedTaskNumber = parseFloat(taskNumber);
+    let taskSum = convertedTaskNumber - 1;
+    document.getElementById("task_n").innerText = taskSum;
+
+    let navTask = document.getElementById("nav_task").innerText;
+    let convertedNavTask = parseInt(navTask);
+    let navSum = convertedNavTask + 1;
+    document.getElementById("nav_task").innerText = navSum;
+
+    document.getElementById("completed-2").style.backgroundColor = "#8D8E9A";
+
+    alert("Board updated successfully")
+
+    // add box
+
+    let box = document.createElement("div");
+    box.style.width = "300px";
+    box.style.height = "100px";
+    box.style.backgroundColor = "#F4F7FF";
+    box.style.margin = "25px";
+    box.style.borderRadius = "15px";
+    box.textContent = `You have Complete The Task Add Dark Mode at ` + new Date().toLocaleTimeString();
+    box.style.color = "gray";
+    box.style.display = "flex"; 
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.textAlign= "center";
+    document.getElementById("container").appendChild(box);
+  });
+
+document
+  .getElementById("completed-3")
+  .addEventListener("click", function (event) {
+    let taskNumber = document.getElementById("task_n").innerText;
+    let convertedTaskNumber = parseFloat(taskNumber);
+    let taskSum = convertedTaskNumber - 1;
+    document.getElementById("task_n").innerText = taskSum;
+
+    let navTask = document.getElementById("nav_task").innerText;
+    let convertedNavTask = parseInt(navTask);
+    let navSum = convertedNavTask + 1;
+    document.getElementById("nav_task").innerText = navSum;
+
+    document.getElementById("completed-3").style.backgroundColor = "#8D8E9A";
+
+    alert("Board updated successfully")
+
+    // box add
+
+    let box = document.createElement("div");
+    box.style.width = "300px";
+    box.style.height = "100px";
+    box.style.backgroundColor = "#F4F7FF";
+    box.style.margin = "25px";
+    box.style.borderRadius = "15px";
+    box.textContent = `You have Complete The Task Optimize Home page at ` + new Date().toLocaleTimeString();
+    box.style.color = "gray";
+    box.style.display = "flex"; 
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.textAlign= "center";
+    document.getElementById("container").appendChild(box);
+  });
+
+document
+  .getElementById("completed-4")
+  .addEventListener("click", function (event) {
+    let taskNumber = document.getElementById("task_n").innerText;
+    let convertedTaskNumber = parseFloat(taskNumber);
+    let taskSum = convertedTaskNumber - 1;
+    document.getElementById("task_n").innerText = taskSum;
+
+    let navTask = document.getElementById("nav_task").innerText;
+    let convertedNavTask = parseInt(navTask);
+    let navSum = convertedNavTask + 1;
+    document.getElementById("nav_task").innerText = navSum;
+
+    document.getElementById("completed-4").style.backgroundColor = "#8D8E9A";
+
+    alert("Board updated successfully")
+
+    // box add
+
+    let box = document.createElement("div");
+    box.style.width = "300px";
+    box.style.height = "100px";
+    box.style.backgroundColor = "#F4F7FF";
+    box.style.margin = "25px";
+    box.style.borderRadius = "15px";
+    box.textContent = `You have Complete The Task Add new emoji at ` + new Date().toLocaleTimeString();
+    box.style.color = "gray";
+    box.style.display = "flex"; 
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.textAlign= "center";
+    document.getElementById("container").appendChild(box);
+  });
+
+document
+  .getElementById("completed-5")
+  .addEventListener("click", function (event) {
+    let taskNumber = document.getElementById("task_n").innerText;
+    let convertedTaskNumber = parseFloat(taskNumber);
+    let taskSum = convertedTaskNumber - 1;
+    document.getElementById("task_n").innerText = taskSum;
+
+    let navTask = document.getElementById("nav_task").innerText;
+    let convertedNavTask = parseInt(navTask);
+    let navSum = convertedNavTask + 1;
+    document.getElementById("nav_task").innerText = navSum;
+
+    document.getElementById("completed-5").style.backgroundColor = "#8D8E9A";
+
+    alert("Board updated successfully")
+
+    // box add
+
+    let box = document.createElement("div");
+    box.style.width = "300px";
+    box.style.height = "100px";
+    box.style.backgroundColor = "#F4F7FF";
+    box.style.margin = "25px";
+    box.style.borderRadius = "15px";
+    box.textContent = `You have Complete The Task Integrate OpenAI API at ` + new Date().toLocaleTimeString();
+    box.style.color = "gray";
+    box.style.display = "flex"; 
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.textAlign= "center";
+    document.getElementById("container").appendChild(box);
+  });
+
+document
+  .getElementById("completed-6")
+  .addEventListener("click", function (event) {
+    let taskNumber = document.getElementById("task_n").innerText;
+    let convertedTaskNumber = parseFloat(taskNumber);
+    let taskSum = convertedTaskNumber - 1;
+    document.getElementById("task_n").innerText = taskSum;
+
+    let navTask = document.getElementById("nav_task").innerText;
+    let convertedNavTask = parseInt(navTask);
+    let navSum = convertedNavTask + 1;
+    document.getElementById("nav_task").innerText = navSum;
+
+    document.getElementById("completed-6").style.backgroundColor = "#8D8E9A";
+
+    alert("Board updated successfully")
+    
+    // add box
+    let box = document.createElement("div");
+    box.style.width = "300px";
+    box.style.height = "100px";
+    box.style.backgroundColor = "#F4F7FF";
+    box.style.margin = "25px";
+    box.style.borderRadius = "15px";
+    box.textContent = `You have Complete The Task Improve Job searching at ` + new Date().toLocaleTimeString();
+    box.style.color = "gray";
+    box.style.display = "flex"; 
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.textAlign= "center";
+    document.getElementById("container").appendChild(box);
+  });
+
+  document.getElementById("clear_btn").addEventListener("click",
+    function (event) {
+        document.getElementById("container").innerHTML = "";
     }
-)
+  )
