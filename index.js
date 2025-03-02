24948abca16fc6cb054f776eea67a7d41381b346
Rahat-khan117 <rahatkhan015002@gmail.com>
2025-03-02 15:51:08 +0600

    third commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("discover_btn").addEventListener("click",
+    function (question) {
+        window.location.href="main.html"
+    }
+)
---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("back_btn").addEventListener("click",
+    function (back) {
+        window.location.href="index.html"
+    }
+)
